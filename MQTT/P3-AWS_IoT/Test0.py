f15a5717b89b376a37221f826be76132919cfcfc
nilang-agarwal <92906382+nilang-agarwal@users.noreply.github.com>
2024-10-04 17:33:20 +0530

    Update Test0.py

--- MQTT/P3-AWS_IoT/Test0.py
+++ MQTT/P3-AWS_IoT/Test0.py
@@ -2,26 +2,22 @@ import paho.mqtt.client as mqtt
 import ssl
 import time
 
-# AWS IoT parameters
 broker_address = "a3c7k5ajyxpwnh-ats.iot.ap-northeast-1.amazonaws.com"  # e.g., "xxxxxxxxxx.iot.us-east-1.amazonaws.com"
 port = 8883
 
 client = mqtt.Client()
 
-# Set up TLS
 client.tls_set(r"C:\Users\nilagarw\Downloads\MyDoorLock2\AmazonRootCA1.pem", 
                 certfile=r"C:\Users\nilagarw\Downloads\MyDoorLock2\1ec0841b1eedab8b126ec35b08516b3618d20ce7d780defe7aa6f7247e544813-certificate.pem.crt", 
                 keyfile=r"C:\Users\nilagarw\Downloads\MyDoorLock2\1ec0841b1eedab8b126ec35b08516b3618d20ce7d780defe7aa6f7247e544813-private.pem.key", 
                 tls_version=ssl.PROTOCOL_TLSv1_2)
 
-# Connect to the broker
 try:
     client.connect(broker_address, port)
-    client.loop_start()  # Start the loop to process network traffic
+    client.loop_start()  
 except Exception as e:
     print("Connection error:", str(e))
 
-# Publish a message
 topic = "India/Bihar/Patna"
 
 message = """{
@@ -88,9 +84,7 @@ message = """{
 
 client.publish(topic, message)
 
-# Optional: Wait a bit to ensure the message is sent before disconnecting
 time.sleep(2)
 
-# Stop the loop and disconnect
 client.loop_stop()
 client.disconnect()
